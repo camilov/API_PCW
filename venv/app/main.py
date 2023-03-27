d515849e9be7e2722ee36e6082887e917bbbbe5f
cristian <cagudelovillegas@gmail.com>
2023-03-26 21:01:52 -0500

    clientes like ruta

--- venv/app/main.py
+++ venv/app/main.py
@@ -17,19 +17,4 @@ app.include_router(auth_routes,prefix="/api")
 app.include_router(clientes,prefix="/api")
 load_dotenv(find_dotenv())
 
-"""def get_db():
-    try:
-        db = SessionLocal()
-        yield db
-    finally:
-        db.close
-
-@app.get("/")
-def main():
-    return RedirectResponse(url="/docs/")
-
-@app.get('/clientes/',response_model=List[schemas.Clientes])
-def show_clientes(db:Session=Depends(get_db)):
-    clientes = db.query(models.Clientes).all()
-    return clientes"""
 
